1f8a5307e51cb727d9d75b93d9cdcecada1828ad
Quique9723 <enriquefdez97@gmail.com>
2024-04-15 11:04:16 +0200

    Reacting to events

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header.jsx'
+import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcept.jsx';
-
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
@@ -22,6 +22,16 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[3]}/>
          
         </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Prop</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+
         </section>
       </main>
     </div>
